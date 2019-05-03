42e6c6c2d545cc50001af1a43741c2aee550e83e
Shad Beard <shadbeard81@gmail.com>
2019-05-03 14:29:28 -0400

    Creating backup scripts for cassandra on aws

---
+++ backup-full.sh
@@ -0,0 +1,1 @@
+/opt/cassandra-aws-backup/cassandra-cloud-backup.sh -b s3://losant_cassandra_backups -vCc -N 10 >> /var/log/cassandra/backup-full.log 2>&1
---
+++ backup-incremental.sh
@@ -0,0 +1,1 @@
+/opt/cassandra-cloud-backup/cassandra-cloud-backup.sh -b gs://losant_cassandra_backups -vi -N 10 >> /var/log/cassandra/backup-incremental.log 2>&1
